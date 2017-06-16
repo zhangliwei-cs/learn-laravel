0a7b2cb52b2ae7f35bbcc479ecb909093a160323
167-3114006271-张立伟 <18826136742@163.com>
2017-06-16 21:08:47 +0800

    Database/geting start

--- app/Http/Controllers/LearnController.php
+++ app/Http/Controllers/LearnController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Database\QueryException;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
@@ -9,5 +10,53 @@ class LearnController extends Controller
 {
     public function index()
     {
+        $this->learn_query();
+    }
+
+    /**
+     * running raw sql queries
+     */
+    public function learn_query(){
+        //running a select query
+        //The select method will always return an array of results. Each result within the array will be a PHP StdClass object.
+        /*$articles = DB::select('select * from levy_articles where art_id > ? or art_id < ?', [20, 15]);
+        dd($articles);*/
+
+        //Using Named Bindings
+        /*$articles = DB::select('select * from levy_articles where art_id > :id1 or art_id < :id2', ['id1'=>20, 'id2'=>15]);
+        dd($articles);*/
+
+
+        //Running An Insert Statement 捕获异常
+        /*try{
+            $res = DB::insert('insert into levy_categories(cate_name) values(?)', ['TEST5']);
+            var_dump($res);
+        }
+        catch (QueryException $ex){
+            dd($ex);
+        }*/
+
+        //Database Transactions
+        /*try{
+            DB::transaction(function(){
+                DB::table('friendlinks')->insert(['link_name' => 'test', 'link_url' => 'test']);
+                DB::table('categories')->insert(['cate_name' => 'TEST5']);
+            });
+        }
+        catch (QueryException $ex){
+            dd($ex);
+        }*/
+        //beginTransaction,rollback,commit
+        /*DB::beginTransaction();
+        DB::table('friendlinks')->insert(['link_name' => 'test', 'link_url' => 'test']);
+        try{
+            DB::table('categories')->insert(['cate_name' => 'TEST5']);
+        }
+        catch (QueryException $ex){
+            DB::rollback();
+        }
+        DB::commit();*/
+
+
     }
 }
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 
 use App\TestB;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -20,6 +21,13 @@ class AppServiceProvider extends ServiceProvider
         $this->app->bind('App\TestB', function($app){
             return new TestB($app->make('App\TestA'));
         });
+
+
+        // Listening For Query Events
+        /*DB::listen(function($query){
+            dd($query);
+        });*/
+
     }
 
     /**
